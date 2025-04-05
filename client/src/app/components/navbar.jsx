7a90996fb20aeb6e0ac6fd7b3f8d60747d45cc05
Nick Swetlin <nswetlin@ucsd.edu>
2025-04-05 15:52:00 -0700

    grid layout complete

---
+++ client/src/app/components/navbar.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NavBar() {
+    return (
+        <div className="bg-white">
+        <h1 className="text-2xl font-bold">NavBar Component</h1>
+        {/* Add your chat component content here */}
+        </div>
+    );
+}
--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,16 +1,21 @@
 import Chat from "./components/chat";
+import NavBar from "./components/navbar";
+import Avatar from "./components/avatar";
 
 
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen grid grid-rows-[40%_60%] gap-4 p-4">
-      <div className="bg-gray-100 p-4 rounded shadow">
-        <h2 className="text-lg font-bold mb-2">Left Panel</h2>
-        <p>This is the left panel content.</p>
+    <div className="bg-white min-h-screen grid grid-rows-[10%_90%] grid-cols-[70%_30%] p-4 overflow-hidden h-full max-h-full">
+      <div className="bg-blue-100 p-4 rounded shadow row-span-1 col-span-2 border border-blue-900">
+        <NavBar />
       </div>
 
-      <div className="bg-gray-100 p-4 rounded shadow row-span-[60%]">
+      <div className="bg-white-100 p-4 rounded shadow row-span-1 col-span-1">
         <Chat />
+      </div>
+
+      <div className="bg-white-100 p-4 rounded shadow row-span-1 col-span-1">
+        <Avatar />
       </div>
     </div>
 
